b66c7f6f3a24a625967e1e35e3ee73abc6001b29
munyasya21 <munyasyacynthia@gmail.com>
2024-01-18 00:22:11 +0300

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,16 @@ const Navbar = () => {
     <nav className="fixed w-full h-4 flex      ">
       
 
-      <div className="     ">
-        <ul className=" hidden sm:flex pt-[36px] gap-[108px] pl-36 pb-[66px] ">
-          <li>Home</li>
+      
+         <div className=" hidden md:block text-[#ADB7BE] hover:text-white">
+         <ul className=' flex-wrap hidden sm:flex pt-[36px] gap-[108px] pl-36 pb-[66px]  '>
+        <li> Home</li>
           <li>Portfolio</li>
           <li>AboutMe</li>
           <li>ContactMe</li>
         </ul>
       </div>
+      
     </nav>
   )
 }
