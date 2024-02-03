19bf0d806f13738096777b6159d133f90c8cc742
munyasya21 <munyasyacynthia@gmail.com>
2024-02-03 21:58:18 +0300

    updated image

--- src/pages/index.js
+++ src/pages/index.js
@@ -190,7 +190,9 @@ export default function Home() {
         </div>
       </div>
       {/* Email */}
-      <div className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+      <div className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-0 -left-4 transform -translate-x-1/2 -translate-1/2 ">
+        </div>
         <div className="pl-[62px]">
           <h5 className="text-xl font-bold text-white my-2 ">Let's Connect</h5>
           <p className="text-[#ADB7BE] mb-4 max-w-md">
