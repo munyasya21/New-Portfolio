3c07bd16a9ff6199337f5306a56cdc55ef44d17c
munyasya21 <munyasyacynthia@gmail.com>
2024-02-06 22:15:07 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,65 @@
 import React from "react";
 import { Link } from "react-scroll";
 
+
 const Navbar = () => {
+ 
+  
+
+  const closeMenu = () => setClick(false);
+
   return (
     <nav className=" flex fixed items-center justify-end w-[92%] mx-auto ">
+     
       <div>
-        <ul className="flex items-center gap-[4vw]  ">
+        <ul className="flex items-center gap-[4vw]">
           <li className="hover:text-gray-500">
-            <a href="#">Home</a>
+            <Link
+              to="/"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Home
+            </Link>
           </li>
           <li className="hover:text-gray-500">
-            <a href="#">AboutMe</a>
+            <Link
+              to="about me"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              AboutMe
+            </Link>
           </li>
           <li className="hover:text-gray-500">
-            <a href="#">My Work</a>
+            <Link
+              to="my work"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              My Work
+            </Link>
           </li>
           <li className="hover:text-gray-500">
-            <a href="#">ContactMe</a>
+            <Link
+              to="contact me"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              ContactMe
+            </Link>
           </li>
         </ul>
       </div>
