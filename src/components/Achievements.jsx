46b8cf2bcafaccfc0da274bf673d31b587f65f43
munyasya21 <munyasyacynthia@gmail.com>
2024-02-16 23:08:53 +0300

    updated animations

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -12,21 +12,21 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: "10",
+    value: "5",
     postfix: "+",
   },
   {
     prefix: "~",
     metric: "Users",
-    value: "100000",
+    value: "1000",
   },
   {
     metric: "Awards",
-    value: "7",
+    value: "3",
   },
   {
     metric: "Years",
-    value: "5",
+    value: "4",
   },
 ];
 
