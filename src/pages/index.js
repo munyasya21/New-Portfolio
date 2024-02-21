10597e5c65a9c99094480fc207088291a19b88b6
Denis <denokim55@gmail.com>
2024-02-21 23:28:50 +0300

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,40 +41,28 @@ export default function Home() {
             />
           </h1>
           <p className="text-[#ADB7BE] text-lg lg:text-xl py-3 ">
-            As a front-end software developer, I excel at transforming design
-            concepts into <br />
+            As a front-end software developer, I excel at transforming design concepts into <br />
             captivating and functional user interfaces.
           </p>
           <div className="flex gap-[16px] py-4">
-            <button className=" rounded-full px-6 py-3 w-full sm:w-fit text-white bg-black hover:bg-slate-200 bg-gradient-to-br from-blue-500 via-purple-500  to-pink-500">
+            <button className=" rounded-full px-6 py-3 whitespace-nowrap w-fit text-white bg-black hover:bg-slate-200 bg-gradient-to-br from-blue-500 via-purple-500  to-pink-500">
               HIRE ME
             </button>
 
-            <button className="rounded-full px-1 py-1 w-full sm:w-fit  text-white hover:bg-slate-200  bg-gradient-to-br from-blue-500 via-purple-500  to-pink-500 ">
+            <button className="rounded-full px-1 py-1 w-fit  text-white hover:bg-slate-200  bg-gradient-to-br from-blue-500 via-purple-500  to-pink-500 ">
               <span className="block bg-black hover:bg-slate-800 rounded-full px-5 py-2">
-              <a
-                target="_blank"
-                rel="noopener"
-                href="c:\Users\CYNTHIA\Documents\My Resume (1).pdf"
-              >
-                DOWNLOAD CV
+                <a target="_blank" rel="noopener" href="c:\Users\CYNTHIA\Documents\My Resume (1).pdf">
+                  DOWNLOAD CV
                 </a>
-                
               </span>
             </button>
           </div>
         </div>
 
         {/* image to the right */}
-        <div className="pl-[50px] pb-[150px] gap-[30px] place-self-center  ">
+        <div className="pl-[50px] pb-[150px] gap-[30px] place-self-center hidden lg:block ">
           <div className=" w-[400px] h-[400px] py-4   ">
-            <Image
-              src={Laptopcode}
-              width={400}
-              height={400}
-              className=" rounded-xl  "
-              alt="laptop code image"
-            />
+            <Image src={Laptopcode} width={400} height={400} className=" rounded-xl  " alt="laptop code image" />
           </div>
         </div>
       </div>
@@ -94,23 +82,12 @@ export default function Home() {
       </div>
       <div className="pl-[70px]">
         <div className="flex w-full gap-[32px] rounded-t-xl rounded-b-xl relative group  ">
-          <script
-            type="module"
-            src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-          ></script>
+          <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 
-          <div className="pt-[55px] flex    flex-col justify-center ">
-            <div className="h-12 w-12 mr-2 border-2 relative rounded-full bg-black border-black hover:border-black top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-black">
-              <a
-                target="_blank"
-                rel="noopener"
-                href="https://quiet-biscochitos-fb2a73.netlify.app"
-              >
-                <ion-icon
-                  className="absolute"
-                  size="large"
-                  name="code-slash-outline"
-                ></ion-icon>
+          <div className="pt-[55px] flex  hover:scale-105 transition-all ease-in-out duration-200  flex-col justify-center ">
+            <div className="p-1 w-fit  grid place-content-center  border-2 relative rounded-full bg-black border-black hover:border-black top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-black">
+              <a target="_blank" rel="noopener" href="https://quiet-biscochitos-fb2a73.netlify.app" className="h-fit">
+                <ion-icon className="absolute" size="large" name="code-slash-outline"></ion-icon>
               </a>
             </div>
             <Image
@@ -124,11 +101,7 @@ export default function Home() {
 
           <div className="pt-[55px] flex flex-col  rounded-2xl   justify-center ">
             <div className="h-12 w-12 mr-2 border-2 relative rounded-full bg-black border-black hover:border-black top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-black">
-              <a
-                target="_blank"
-                rel="noopener"
-                href="https://stellar-twilight-40137a.netlify.app"
-              >
+              <a target="_blank" rel="noopener" href="https://stellar-twilight-40137a.netlify.app">
                 <ion-icon size="large" name="code-slash-outline"></ion-icon>
               </a>
             </div>
@@ -143,38 +116,24 @@ export default function Home() {
         </div>
       </div>
       {/* Email */}
-      <div
-        id="contact"
-        className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-      >
+      <div id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute  top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2 "></div>
         <div className="pl-[62px]">
           <h5 className="text-xl font-bold text-white my-2 ">Let's Connect</h5>
           <p className="text-[#ADB7BE] mb-4 max-w-md">
             {""}
-            I'm currently looking for new opportunities, my inbox is always
-            open. Whether you have a question or not, I'll try my best to get
-            back to you!
+            I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or not,
+            I'll try my best to get back to you!
           </p>
-          <script
-            type="module"
-            src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-          ></script>
+          <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
           <div className=" flex gap-2 w-8 h-8">
             <div className="w-8 h-8">
-              <a
-                target="_blank"
-                rel="noopener"
-                href="https://github.com/munyasya21"
-              >
-              <ion-icon size="large" name="logo-github"></ion-icon></a>
+              <a target="_blank" rel="noopener" href="https://github.com/munyasya21">
+                <ion-icon size="large" name="logo-github"></ion-icon>
+              </a>
             </div>
             <div className="w-8 h-8">
-              <a
-                target="_blank"
-                rel="noopener"
-                href="https://www.linkedin.com/in/cynthia-munyasya-b14676245"
-              >
+              <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/cynthia-munyasya-b14676245">
                 <ion-icon size="large" name="logo-linkedin"></ion-icon>
               </a>
             </div>
@@ -182,10 +141,7 @@ export default function Home() {
         </div>
         <form className="flex flex-col  ">
           <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="text-white mb-2 block text-sm font-medium"
-            >
+            <label htmlFor="email" className="text-white mb-2 block text-sm font-medium">
               Your Email
             </label>
             <input
@@ -197,10 +153,7 @@ export default function Home() {
             />
           </div>
           <div className="mb-6">
-            <label
-              htmlFor="subject"
-              className="text-white block mb-2  text-sm font-medium"
-            >
+            <label htmlFor="subject" className="text-white block mb-2  text-sm font-medium">
               Subject
             </label>
             <input
@@ -212,10 +165,7 @@ export default function Home() {
             />
           </div>
           <div className="mb-6">
-            <label
-              htmlFor="message"
-              className="text-white block mb-2  text-sm font-medium"
-            >
+            <label htmlFor="message" className="text-white block mb-2  text-sm font-medium">
               Message
             </label>
             <textarea
